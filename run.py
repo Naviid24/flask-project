0cdd3d7b3e2b120e719834e6c8288fa76b40ef01
Navid bahadorani <navid.bhd@yahoo.com>
2025-01-11 15:47:11 +0000

    Usead Jinja templating inside the html files and create contact html file

--- run.py
+++ run.py
@@ -15,6 +15,17 @@ def index():
     #flask will looking for temoplates folder to find html file we refrenssed in pranteces
     return render_template("index.html")
 
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
 if __name__ == '__main__':
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
